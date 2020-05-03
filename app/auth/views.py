6d58b3098c1558ec5cc65c7fc199efa081cb194e
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-03 21:19:58 +0300

    added signup and login fuctions

--- app/auth/views.py
+++ app/auth/views.py
@@ -3,3 +3,36 @@ from . import auth
 from flask_login import login_user,logout_user,login_required,current_user
 from ..models import User,Pitch
 from .forms import LoginForm,SignUpFrom
+
+@auth.route('/signup',methods=['GET','POST'])
+def signup():
+    form = SignUpForm()
+    if form.validate_on_submit():
+        user =  User(email = form.email.data, username = form.username.data,password = form.password.data)
+        user.save_user()
+        try:
+            msg = Message('Hello...Welcome to pitches.We are glad you joined us',sender=('owinolawrence21@gmail.com'))
+            msg.add_recipient(user.email)
+            mail.send(msg)
+        except Exception as e:
+            print('failed')
+
+        return redirect(url_for('auth.login'))
+    
+        title = "New Account"
+    return render_template('sign_up.html',signup_form = form)
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username = form.username.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user,form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.all_pitches'))
+
+        else:
+            flash('Invalid username or Password')
+
+
+    return render_template('login.html',login_form = form)
