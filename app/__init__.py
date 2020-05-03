87e208e54f699efe3fe9b503ab2ed316e8a0f7d2
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-03 20:05:28 +0300

    install and import flask-login to __init__.py file

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,14 @@ from flask import Flask
 # from config import Config
 import os
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 # bootstrap = Bootstrap
 
 # Initializing application
 app = Flask(__name__)
+
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
