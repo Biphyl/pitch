98b281dbdd4911551be22838d2a3f6bd9d1edc6d
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-03 20:35:18 +0300

    created class SignUpForm and added signup details

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,3 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField,BooleanField,SubmitField
 from wtforms.validators import DataRequired,Length,Email,EqualTo
+
+class SignUpFrom(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(),Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(),EqualTo('password')])
+    submit = SubmitField('Sign Up')
