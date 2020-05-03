bf8131404186b5383826c6dc636541a17ae6afb6
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-03 20:39:39 +0300

    created class LoginForm and added LoginForm content

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -8,3 +8,9 @@ class SignUpFrom(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(),EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Log In')
