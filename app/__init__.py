a11fd8f2f1429a8ef87782452d3ac7d6034ebe18
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-03 20:07:51 +0300

    initialized the extension to create_app function

--- app/__init__.py
+++ app/__init__.py
@@ -13,3 +13,11 @@ app = Flask(__name__)
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
+
+def create_app(config_name):
+
+
+    # Initializing Flask Extensions
+    
+
+    login_manager.init_app(app)
