1d283da6ebd9047734b9f48eb4c00790b116ff62
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-04 14:02:28 +0300

    modified app/__init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,42 @@
 from flask import Flask
-# from config import Config
+from config import Config
 import os
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from config import config_options
+from flask_bootstrap import Bootstrap
+from flask_uploads import UploadSet,configure_uploads,IMAGES
+from flask_mail import Mail
 
-
-# Initializing application
 app = Flask(__name__)
-# bootstrap = Bootstrap()
+bootstrap = Bootstrap()
 db = SQLAlchemy()
+photos = UploadSet('photos',IMAGES)
+mail = Mail()
+
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
-
-def create_app(config_name):
-
-
-    # Initializing Flask Extensions
-    bootstrap.init_app(app)
-    db.init_app(app)
+def pitch_app(config_name):
+    
+    app.config.from_object(config_options[config_name])
+    
+    # initialize extensions
     login_manager.init_app(app)
+    db.init_app(app)
+    bootstrap.init_app(app)
+    mail.init_app(app)
+
+   # configure UploadSet
+    configure_uploads(app,photos)
+
+   # registering main blueprint
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # registering auth blueprint
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint)
+
+    return app
