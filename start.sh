3662ec2fa52affc2a733bdff089b015b28b89a96
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-05 09:07:22 +0300

    modify secret key

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export SECRET_KEY=435313ea80b5a872114356a1
+export SECRET_KEY='Bilovine'
 export APP_SETTINGS="config.DevelopmentConfig"
 export DATABASE_URL='postgresql+psycopg2://biron:Biron4745@localhost/pitches'
 export MAIL_PASSWORD='Biron4745'
